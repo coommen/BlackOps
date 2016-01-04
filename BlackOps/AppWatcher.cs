a3c2618011917a47b116a52f8878ab7123d0f736
coommen <ciby_o@hotmail.com>
2016-01-03 22:23:39 -0600

    AppWatcher for javaws.exe (hardcoded)

---
+++ BlackOps/AppWatcher.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Management;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BlackOps
+{
+    public partial class AppWatcher : ServiceBase
+    {
+        public System.Management.ManagementEventWatcher mgmtWtch;
+
+        public AppWatcher()
+        {
+            InitializeComponent();
+
+
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            // EventLog.WriteEntry("Service Started", EventLogEntryType.Information);
+            mgmtWtch = new System.Management.ManagementEventWatcher("Select * From Win32_ProcessStartTrace where ProcessName='javaws.exe'");
+            mgmtWtch.EventArrived += new System.Management.EventArrivedEventHandler(mgmtWtch_EventArrived);
+            mgmtWtch.Start();
+        }
+
+        protected override void OnStop()
+        {
+            // EventLog.WriteEntry("Service Stopped", EventLogEntryType.Information);
+            mgmtWtch.Stop();
+        }
+
+        void mgmtWtch_EventArrived(object sender, System.Management.EventArrivedEventArgs e)
+        {
+
+            String processID = e.NewEvent.Properties["ProcessID"].Value.ToString();
+            String processName = e.NewEvent.Properties["ProcessName"].Value.ToString();
+            
+            #region Example to enumerate Properties
+            //foreach (PropertyData pd in e.NewEvent.Properties)
+            //{
+            //    EventLog.WriteEntry(String.Format("{0} : {1}", pd.Name, pd.Value), EventLogEntryType.Warning);
+            //}
+            #endregion
+
+            EventLog.WriteEntry("Detected Process Start: " + processName + " Process ID:" + processID, EventLogEntryType.Warning);
+            stopProcessByID(Convert.ToInt32(processID));
+        }
+
+        void stopProcessByID(int ID)
+        {
+            try
+            {
+                Process process = Process.GetProcessById(ID);
+                String userid = process.StartInfo.UserName;
+                if (userid=="ruben_000")
+                    process.Kill();
+                EventLog.WriteEntry("Stopped Process: " + process.ProcessName + " Process ID:" + process.Id, EventLogEntryType.Warning);
+            }
+            catch(Exception ex)
+            {
+                EventLog.WriteEntry("Encountered Errors killing Process:\n" + ex.ToString(), EventLogEntryType.Error);
+
+            }
+        }
+
+}
+
+}
+
---
+++ BlackOps/ProjectInstaller.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace BlackOps
+{
+    partial class ProjectInstaller
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.awProcessInstaller = new System.ServiceProcess.ServiceProcessInstaller();
+            this.awInstaller = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // awProcessInstaller
+            // 
+            this.awProcessInstaller.Account = System.ServiceProcess.ServiceAccount.LocalSystem;
+            this.awProcessInstaller.Password = null;
+            this.awProcessInstaller.Username = null;
+            // 
+            // awInstaller
+            // 
+            this.awInstaller.Description = "Application Blacklisting Service";
+            this.awInstaller.DisplayName = "BlackOps App Watcher";
+            this.awInstaller.ServiceName = "AppWatcher";
+            this.awInstaller.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.awProcessInstaller,
+            this.awInstaller});
+
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller awProcessInstaller;
+        private System.ServiceProcess.ServiceInstaller awInstaller;
+    }
+}
